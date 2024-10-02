47a558f062e57149142d9731283726237a2144f1
Rokas <icestar6215@gmail.com>
2024-10-02 17:51:13 +0800

    feat: launches list notifier add resetState and updateFilterData function

--- lib/presentation/pages/launches_list/launches_list_notifier.dart
+++ lib/presentation/pages/launches_list/launches_list_notifier.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:newton_tech_app/presentation/pages/launches_list/launches_list_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../domain/entity/launch_info.dart';
 import '../../../domain/usecases/get_launches_usecase.dart';
 
 abstract class LaunchesListNotifier extends StateNotifier<LaunchesListState> {
   LaunchesListNotifier(super.state);
 
   Future<void> fetchData();
+  void resetState();
+  void updateFilterData({
+    required DateTimeRange dateTimeRange,
+    int? flightNumber,
+    String? missionName,
+  });
 }
 
 class LaunchesListNotifierImpl extends LaunchesListNotifier {
@@ -14,15 +22,84 @@ class LaunchesListNotifierImpl extends LaunchesListNotifier {
 
   LaunchesListNotifierImpl({
     required this.getLaunchesUseCase,
-  }) : super(LaunchesListState.init());
+  }) : super(LaunchesListState.loading());
 
   @override
   Future<void> fetchData() async {
     final result = await getLaunchesUseCase.execute(null);
     result.when(success: (data) {
-      state = state.copyWith(launchesList: data);
+      state = LaunchesListState.loaded(
+        launchesList: data,
+        startTime: DateTime.parse('1970-01-01'),
+        endTime: DateTime.now(),
+        missionName: null,
+        flightNumber: null,
+      );
     }, error: (error) {
-      state = LaunchesListState.init();
+      state = LaunchesListState.error();
     });
+  }
+
+  @override
+  void resetState() {
+    state = LaunchesListState.loading();
+    fetchData();
+  }
+
+  bool checkDateTimeRange(
+    int dateTimeUnix,
+  ) {
+    int unixTimeAfterConvert = dateTimeUnix * 1000;
+    return DateTime.fromMillisecondsSinceEpoch(unixTimeAfterConvert)
+            .isAfter((state as Loaded).startTime) &&
+        DateTime.fromMillisecondsSinceEpoch(unixTimeAfterConvert)
+            .isBefore((state as Loaded).endTime);
+  }
+
+  @override
+  void updateFilterData({
+    required DateTimeRange dateTimeRange,
+    int? flightNumber,
+    String? missionName,
+  }) {
+    List<LaunchInfo> sortedList = [];
+
+    // only DateTime changed
+    if (flightNumber == null && (missionName == null && missionName!.isEmpty)) {
+      (state as Loaded).launchesList.forEach((element) {
+        if (checkDateTimeRange(element.launchDateUnix)) {
+          sortedList.add(element);
+        }
+      });
+      return;
+    }
+
+    (state as Loaded).launchesList.forEach((element) {
+      // flightNumber changed
+      if (flightNumber != null && element.flightNumber == flightNumber) {
+        if (checkDateTimeRange(element.launchDateUnix)) {
+          sortedList.add(element);
+        }
+      }
+
+      // missionName changed
+      if (missionName != null &&
+          missionName.isNotEmpty &&
+          element.missionName
+              .toLowerCase()
+              .contains(missionName.toLowerCase())) {
+        if (checkDateTimeRange(element.launchDateUnix)) {
+          sortedList.add(element);
+        }
+      }
+    });
+
+    state = (state as Loaded).copyWith(
+      launchesList: sortedList,
+      startTime: dateTimeRange.start,
+      endTime: dateTimeRange.end,
+      flightNumber: flightNumber,
+      missionName: missionName,
+    );
   }
 }
