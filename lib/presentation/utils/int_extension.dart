7aab2e1f00f56ca24c5a646f90ef8a3d6f13ab10
Rokas <icestar6215@gmail.com>
2024-10-02 17:48:20 +0800

    chore: remove unused int extension utils

--- lib/presentation/utils/int_extension.dart
+++
@@ -1,6 +0,0 @@
-extension UnixTimeConvertHelper on int {
-  // make unix time format convert to string
-  String timeStringConvertFromUnixTime() {
-    return DateTime.fromMillisecondsSinceEpoch(this * 1000).toIso8601String();
-  }
-}
